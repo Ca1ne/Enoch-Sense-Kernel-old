8acde536433451f7db28ebf54e2ce07dc63bc5af
Ca1ne <Ca1nexda@gmail.com>
2012-07-19 09:15:38 -0400

    fix compile error, remove compile warning

--- block/blk-ioc.c
+++ block/blk-ioc.c
@@ -22,7 +22,7 @@ static void hlist_sched_dtor(struct io_context *ioc, struct hlist_head *list)
 	if (!hlist_empty(list)) {
 		struct cfq_io_context *cic;
 
-		cic = hlist_entry(list->first, struct cfq_io_context, cic_list)
+		cic = hlist_entry(list->first, struct cfq_io_context, cic_list);
 		cic->dtor(ioc);
 	}
 }
